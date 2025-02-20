cfa28749b535e9becf83d56af43334eeb262d09e
jiwon <pg15320@gmail.com>
2025-02-20 16:46:06 +0900

    update search page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-black">
         <Header />
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+          {/* 다른 라우트들... */}
+        </Routes>
       </div>
     </BrowserRouter>
   );
